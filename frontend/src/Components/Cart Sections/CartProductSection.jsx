b3441fc64d21f94576bb56da7b7d46db508da09a
Ashish1820020 <ashishbhattacharyya5@gmail.com>
2023-10-18 10:21:41 +0530

    update edit profile

--- frontend/src/Components/Cart Sections/CartProductSection.jsx
+++ frontend/src/Components/Cart Sections/CartProductSection.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import CartProduct from "./CartProduct";
-import { redirect } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const CartProductSection = () => {
   const cartData = useSelector((state) => state.cart);
   const { isLoggedIn } = useSelector((state) => state.auth);
   const { cartProducts } = cartData;
+  const navigate = useNavigate();
 
   return (
     <>
@@ -14,7 +15,7 @@ const CartProductSection = () => {
         <CartProduct key={index} currentElement={currentElement} />
       ))}
       <div className="order-btn">
-          <button onClick={()=> redirect(isLoggedIn ? "/cart/checkout" : "/loginsignup")}>Place Order</button>
+          <button onClick={()=> navigate(isLoggedIn ? "/cart/checkout" : "/loginsignup")}>Place Order</button>
       </div>
     </>
   );
